5b01b3896beafce59e6db8323229e49e4e5fea82
xiang <xiangquan3721@163.com>
2019-10-09 10:42:38 +0800

    2019年10月8日和气先生提出，在登录模式时，退出车库的自动模式

--- EEPROM.c
+++ EEPROM.c
@@ -130,6 +130,14 @@ void ID_learn(void)
              TIME_Login_EXIT_Button=500;   //2015.3.23ÐÞ¸Ä
          }
          if((FLAG_ID_Erase_Login==1)||(FLAG_ID_Login==1)){
+             
+                FG_auto_manual_mode = 0;
+                TIMER1s=0;
+			    FG_auto_out=0;
+                TIME_auto_useful = 0;
+			    TIME_auto_close=0; 
+                Manual_override_TIMER=0;                           
+
              TIME_Receiver_Login_led++;
              if(TIME_Receiver_Login_led>=45){              //2015.3.23ÐÞ¸Ä
                  TIME_Receiver_Login_led=0;
